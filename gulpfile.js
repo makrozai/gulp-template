ec557df59ddbea7cf7ed930ab8d660cb44cbf68e
makrozai <programador2@iMac-de-La-2.local>
2019-06-03 11:44:57 -0500

    configura gulpfile y tareas de framework

--- gulpfile.js
+++ gulpfile.js
@@ -15,37 +15,47 @@ var gulp = require('gulp'),
 gulp.task('default', ['uglify', 'scss', 'pug', 'imagemin', 'fontmin', 'webserver', 'watch']);
 
 // compila archivos ES6 en ES5
-gulp.task('uglify', function() {
-    gulp.src('source/js/*.js')
-        .pipe(plumber())
-        .pipe(babel({
-          presets: ['es2015']
-        }))
-        .pipe(uglify())
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/js'));
+gulp.task('sitejs', function() {
+  gulp.src('source/js/*.js')
+    .pipe(plumber())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
 });
+
+// agrega tarea para minificar archivos js de framework materialize
+gulp.task('frameworkjs', function() { 
+  gulp.src('source/js/materialize/*.js')
+  .pipe(concat('materialize.js'))
+  .pipe(gulp.dest('dist/js'));
+})
+
+//agrupa 2 tareas de js
+gulp.task('uglify',['sitejs','frameworkjs']);
 
 // compilar archivos SCSS  en CSS
 gulp.task('scss', function() {
-    gulp.src('source/scss/*.scss')
-        .pipe(plumber())
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer({browsers: ['last 3 versions', '> 5%', 'Firefox ESR']}))
-        .pipe(gulp.dest('dist/css'));
+  gulp.src('source/scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({browsers: ['last 3 versions', '> 5%', 'Firefox ESR']}))
+    .pipe(gulp.dest('dist/css'));
 });
 
 // compila archivos PUG(JADE) en HTML
 gulp.task('pug', function() {
-    gulp.src('source/templates/*.pug')
-        .pipe(plumber())
-        .pipe(pug({pretty: true}))
-        .pipe(gulp.dest('./'));
+  gulp.src('source/templates/*.pug')
+    .pipe(plumber())
+    .pipe(pug({pretty: true}))
+    .pipe(gulp.dest('./'));
 });
 
 // comprime archivos de imagen(JPG,JPEG, PNG,GIF,SVG)
 gulp.task('imagemin', function() {
-    gulp.src('source/img/**/*.{jpg,jpeg,png,gif,svg}')
+  gulp.src('source/img/**/*.{jpg,jpeg,png,gif,svg}')
     .pipe(plumber())
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
@@ -53,7 +63,7 @@ gulp.task('imagemin', function() {
 
 // genera archivos de fuente compatibles a partir de TTF
 gulp.task('fontmin', function() {
-    gulp.src('source/fonts/**/*.{ttf,eot,svg,woff}')
+  gulp.src('source/fonts/**/*.{ttf,eot,svg,woff}')
     //.pipe(fontmin({text: '天地玄黄 宇宙洪荒'}))
     .pipe(gulp.dest('dist/fonts'));
 });
@@ -70,9 +80,9 @@ gulp.task('webserver', function() {
 
 // escucha todos los cambios
 gulp.task('watch', function() {
-    gulp.watch('source/js/**/*.js', ['uglify']);
-    gulp.watch('source/scss/**/*.scss', ['scss']);
-    gulp.watch('source/templates/**/*.pug', ['pug']);
-    gulp.watch('source/fonts/**/*.ttf', ['fontmin']);
-    gulp.watch('source/img/*.{jpg,jpeg,png,gif}', ['imagemin']);
+  gulp.watch('source/js/**/*.js', ['uglify']);
+  gulp.watch('source/scss/**/*.scss', ['scss']);
+  gulp.watch('source/templates/**/*.pug', ['pug']);
+  gulp.watch('source/fonts/**/*.ttf', ['fontmin']);
+  gulp.watch('source/img/*.{jpg,jpeg,png,gif}', ['imagemin']);
 });
